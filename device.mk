c39ad70d60902c1150b323ee2be917e671077425
Kartik-91 <kartikbanwalaop@gmail.com>
2023-01-05 10:54:57 +0530

    pissarro: build media libs from source
    Signed-off-by: Kartik-91 <kartikbanwalaop@gmail.com>

--- device.mk
+++ device.mk
@@ -220,6 +220,18 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service
+
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libcodec2_hidl_plugin.vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor \
+    libavservices_minijail \
+    libavservices_minijail_vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
