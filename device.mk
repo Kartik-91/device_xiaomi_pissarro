b902f8c5037eb29c822eb5fef5e15d67129e09b5
Kartik-91 <kartikbanwalaop@gmail.com>
2023-01-06 09:30:47 +0530

    Revert "pissarro: build Thermals and USB libs from source"
    This reverts commit 9fa5a73b92d1585207c3eca6f57b6ff12c470640.

--- device.mk
+++ device.mk
@@ -407,19 +407,6 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0.vendor
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0.vendor \
-    android.hardware.usb@1.1.vendor \
-    android.hardware.usb@1.2.vendor \
-    android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.1.vendor
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-V1-ndk_platform.vendor
