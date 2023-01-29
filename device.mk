0c52d30884352a5c74b1c1b61477c066e3f72d75
Kartik-91 <kartikbanwalaop@gmail.com>
2023-01-29 01:05:15 +0000

    pissarro: Switch android.hardware.bluetooth.audio to AIDL
    Signed-off-by: Kartik-91 <kartikbanwalaop@gmail.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@5.0.vendor \
     android.hardware.audio.common@7.0-util.vendor \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3.vendor \
     android.hardware.soundtrigger@2.0-core \
     android.hardware.soundtrigger@2.3-impl \
